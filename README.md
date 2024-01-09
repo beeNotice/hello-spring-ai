6421e540eebbfaa943d5af8aad72fa81d76db25c
Fabien MARTIN <fmartin@vmware.com>
2024-01-09 16:05:09 +0100

    First version

---
+++ README.md
@@ -0,0 +1,30 @@
+
+## Spring Project Initialization
+
+* https://start.spring.io/
+  * 3.2.2 (SNAPSHOT)
+  * azure-open-ai
+
+## [Azure Open AI](https://docs.spring.io/spring-ai/reference/api/clients/azure-openai.html#_gettting_started)
+
+* https://docs.spring.io/spring-ai/reference/getting-started.html
+* https://portal.azure.com/
+  * Generate keys
+  * Add dependency
+  * Configure Keys
+
+## Prompt
+
+* https://github.com/scraly/developers-conferences-agenda
+
+## Tuning
+
+spring.ai.azure.openai.chat.temperature=0
+
+```
+The date of the events are defined as follows:
+* The Month is defined in the ## level as the name
+* The days are the first information of the line of the event
+* It can be a single date, or a period
+* All events have a date
+```
---
+++ azure-open-ai/src/main/java/com/beenotice/azure/SimpleAiService.java
@@ -0,0 +1,55 @@
+package com.beenotice.azure;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.prompt.Prompt;
+import org.springframework.ai.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class SimpleAiService {
+
+    private final ChatClient chatClient;
+
+    @Value("classpath:/prompts/prompt.st")
+    private Resource promptResource;
+
+    @Value("classpath:/prompts/prompt-with-context.st")
+    private Resource promptWithContextResource;
+
+    @Value("classpath:/docs/developers-conferences-agenda.md")
+    private Resource docsToStuffResource;
+
+    public SimpleAiService(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public void run(){
+        helloPrompt();
+        templatedPrompt();
+        stuffedPrompt();
+    }
+
+    public void helloPrompt(){
+        String response = chatClient.generate("What will be the date and place of the Kubecon in 2024?");
+        System.out.println(response);
+    }
+
+    public void templatedPrompt(){
+        PromptTemplate promptTemplate = new PromptTemplate(promptResource);
+        Prompt prompt = promptTemplate.create(Map.of("question", "What will be the date and place of the Kubecon in 2024?"));
+        String response = chatClient.generate(prompt).getGeneration().getContent();
+        System.out.println(response);
+    }
+
+    public void stuffedPrompt(){
+        PromptTemplate promptTemplate = new PromptTemplate(promptWithContextResource);
+        Prompt prompt = promptTemplate.create(Map.of("context", docsToStuffResource, "question", "What will be the date and place of the Kubecon in 2024?"));
+        String response = chatClient.generate(prompt).getGeneration().getContent();
+        System.out.println(response);
+    }
+
+}
